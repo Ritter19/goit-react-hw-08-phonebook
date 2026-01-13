67c77c69a3f367c5464548707caccdfb68e691de
Ritter19 <joey.barbacena@gmail.com>
2026-01-13 17:24:37 +0800

    last update

--- src/index.js
+++ src/index.js
@@ -1,30 +1,4 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom/client';
-// import { BrowserRouter } from 'react-router-dom';
-// import { App } from 'components/App';
-// import './index.css';
-// import { Provider } from 'react-redux';
-// import { PersistGate } from 'redux-persist/integration/react';
-// import { store, persistor } from './redux/store';
 
-// ReactDOM.createRoot(document.getElementById('root')).render(
-//   <React.StrictMode>
-//     <Provider store={store}>
-//       <PersistGate loading={null} persistor={persistor}>
-//         <BrowserRouter
-//           future={{
-//             v7_startTransition: true,
-//             v7_relativeSplatPath: true,
-//           }}
-//         >
-//           <App />
-//         </BrowserRouter>
-//       </PersistGate>
-//     </Provider>
-//   </React.StrictMode>
-// );
-
-// src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
