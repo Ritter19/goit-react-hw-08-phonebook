cae865b17c5b76c8da996b5adf60937b5a2ba934
Ritter19 <joey.barbacena@gmail.com>
2026-01-13 17:17:12 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import { BrowserRouter } from 'react-router-dom';
+// import { App } from 'components/App';
+// import './index.css';
+// import { Provider } from 'react-redux';
+// import { PersistGate } from 'redux-persist/integration/react';
+// import { store, persistor } from './redux/store';
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <Provider store={store}>
+//       <PersistGate loading={null} persistor={persistor}>
+//         <BrowserRouter
+//           future={{
+//             v7_startTransition: true,
+//             v7_relativeSplatPath: true,
+//           }}
+//         >
+//           <App />
+//         </BrowserRouter>
+//       </PersistGate>
+//     </Provider>
+//   </React.StrictMode>
+// );
+
+// src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
@@ -12,6 +39,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <BrowserRouter
+          basename={process.env.PUBLIC_URL}
           future={{
             v7_startTransition: true,
             v7_relativeSplatPath: true,
